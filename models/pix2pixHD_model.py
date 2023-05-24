db6d94f8b9d04106f8c390d3b6fedc7cf9aa12e2
Seppo Enarvi <seppo.git@marjaniemi.com>
2023-05-24 09:46:04 +0300

    Fixed, works with one output channel
    - Works with the latest PyTorch.
    - Works with the latest Torchvision.
    - Using one output channel works.

--- models/pix2pixHD_model.py
+++ models/pix2pixHD_model.py
@@ -151,7 +151,7 @@ class Pix2PixHDModel(BaseModel):
 
     def forward(self, label, inst, image, feat, infer=False):
         # Encode Inputs
-        input_label, inst_map, real_image, feat_map = self.encode_input(label, inst, image, feat)  
+        input_label, inst_map, real_image, feat_map = self.encode_input(label, inst, image, feat)
 
         # Fake Generation
         if self.use_features:
@@ -166,7 +166,7 @@ class Pix2PixHDModel(BaseModel):
         pred_fake_pool = self.discriminate(input_label, fake_image, use_pool=True)
         loss_D_fake = self.criterionGAN(pred_fake_pool, False)        
 
-        # Real Detection and Loss        
+        # Real Detection and Loss
         pred_real = self.discriminate(input_label, real_image)
         loss_D_real = self.criterionGAN(pred_real, True)
 
